c1250de459813f1e804114d4714015cccb6b2a19
William Oliveira <sudowilliam@gmail.com>
2014-09-14 17:17:31 -0300

    Draw background

--- web/flappydart.dart
+++ web/flappydart.dart
@@ -5,8 +5,15 @@ import 'dart:async';
 
 part 'sprite.dart';
 
+const String BACKGROUND = "assets/background.png";
+
+const int CANVAS_WIDTH = 960;
+const int CANVAS_HEIGHT = 780;
+
 CanvasElement canvas;
 Timer t;
+
+ImageElement background;
 
 void update(Timer t) {
     draw();
@@ -14,10 +21,19 @@ void update(Timer t) {
 
 void draw() {
     CanvasRenderingContext2D canvasContext = canvas.context2D;
+    
+    canvasContext.drawImage(background, 0, 0);
+}
+
+void init() {
+    background = new ImageElement();
+    background.src = BACKGROUND;
 }
 
 void main() {
     canvas = querySelector("#flappyCanvas");
+    
+    init();
     
     t = new Timer.periodic(new Duration(milliseconds: 17), update);
 }
